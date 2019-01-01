bd102a577ae6dd2538cbcec55270da00f73956a4
neuralvfx <neuralvfx@gmail.com>
2019-01-01 18:23:58 +0800

    making naming for perceptual loss consistent

--- models/networks.py
+++ models/networks.py
@@ -92,8 +92,8 @@ class TransposeBlock(nn.Module):
             padding = int(kernel_size // 2 // stride)
 
         operations = []
-        operations += nn.ConvTranspose2d(in_channels=ic, out_channels=oc, padding=padding, output_padding=0,
-                                         kernel_size=kernel_size, stride=stride, bias=False)
+        operations += [nn.ConvTranspose2d(in_channels=ic, out_channels=oc, padding=padding, output_padding=0,
+                                         kernel_size=kernel_size, stride=stride, bias=False)]
 
         operations += [nn.LeakyReLU(inplace=True), nn.BatchNorm2d(oc), nn.Dropout(drop)]
 
