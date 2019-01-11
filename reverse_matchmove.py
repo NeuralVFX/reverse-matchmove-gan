a6dac50fa48243e517fdf016cb7b12b4f126ccac
neuralvfx <neuralvfx@gmail.com>
2019-01-11 23:59:41 +0800

    test disc kernel size again

--- reverse_matchmove.py
+++ reverse_matchmove.py
@@ -175,10 +175,8 @@ class ReverseMatchmove:
         state = torch.load(filepath)
 
         for i in self.model_dict.keys() :
-            if i in state['models'].keys() and i != 'D':
                 self.model_dict[i].load_state_dict(state['models'][i])
         for i in self.opt_dict.keys():
-            if i in state['optimizers'].keys() and i != 'D':
                 self.opt_dict[i].load_state_dict(state['optimizers'][i])
         if not reset:
             self.current_iter = state['iter'] + 1
