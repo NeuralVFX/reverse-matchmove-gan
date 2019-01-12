edd21cd73bf493987f6cb92b9afe28295de894ab
neuralvfx <neuralvfx@gmail.com>
2019-01-12 12:11:39 +0800

    allow more steps of training for discriminator

--- models/networks.py
+++ models/networks.py
@@ -211,7 +211,7 @@ class UnshuffleDiscriminator(nn.Module):
         filt_count = filts_min
 
         for a in range(layers):
-            operations += [DownRes(ic=min(filt_count, filts), oc=min(filt_count * 2, filts), kernel_size=5)]
+            operations += [DownRes(ic=min(filt_count, filts), oc=min(filt_count * 2, filts), kernel_size=3)]
             print(min(filt_count * 2, filts))
             filt_count = int(filt_count * 2)
 
