85632fd44d73cc8d11a2fb89897ff9b94d2bb0e4
neuralvfx <neuralvfx@gmail.com>
2019-01-27 19:17:38 +0800

    test adding attn

--- models/networks.py
+++ models/networks.py
@@ -184,7 +184,7 @@ class Generator(nn.Module):
         for a in range(layers):
             print ('up_block')
             operations += [UpResBlock(int(min(max_filts, filt_count * 2)), int(min(max_filts, filt_count)), drop=drop)]
-            if a == 2:
+            if a == 1:
                 print('attn')
                 operations += [SelfAttention(int(min(max_filts, filt_count * 2)))]
             filt_count = int(filt_count * 2)
